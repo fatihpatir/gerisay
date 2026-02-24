60fa3a272b57f95a76c320bf6c8af2be8ddcbde8
fatihpatir <fatihpatir.hoca@gmail.com>
2026-02-24 10:39:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
-self.addEventListener('fetch', (event) => {
-  // Çevrimdışı destek için boş fetch dinleyicisi
+const CACHE_NAME = 'gerisay-v1';
+const assets = ['./', './index.html', './manifest.json', './icon.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
